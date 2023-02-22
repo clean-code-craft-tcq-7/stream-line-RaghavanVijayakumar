aa42677a085d225e82de6fc2e58fef9214621878
deepakp-31 <deepakp3101@gmail.com>
2023-02-23 01:24:24 +0530

    added test case for min and max

--- test_Receive_Battery_Charging_Parameters.cpp
+++ test_Receive_Battery_Charging_Parameters.cpp
@@ -4,27 +4,27 @@
 
 TEST_CASE("Test case for Received Battery paramter within limits")
 {
-    float min_val = 0.0;
-    float max_val = 95.0;
+  float min_val = 0.0;
+  float max_val = 95.0;
 
-    std::vector<float> temperature_reading = {0.0, 45.1, 45.0, 10.1, 3.0, 7.5, 8.7, 23.4, 56.8, 3.2,
-                                              3, 20.0, 80.1, 80.0, 56.0, 45.8, 9.1, 23.7, 10.5, 60.2,
-                                              45.0, 10.1, 3.0, 7.5, 73.5, 80.1, 80.0, 56.0, 45.8, 90.4,
-                                              10.1, 3.0, 7.5, 8.7, 23.4, 4.2, 30.5, 18.5, 56.2, 62.4,
-                                              45.8, 9.1, 23.7, 7.3, 34.4, 35.8, 45.8, 52.6, 17.5, 23.6};
+  std::vector<float> temperature_reading = { 0.0,  45.1, 45.0, 10.1, 3.0,  7.5,  8.7,  23.4, 56.8, 3.2,
+                                             3,    20.0, 80.1, 80.0, 56.0, 45.8, 9.1,  23.7, 10.5, 60.2,
+                                             45.0, 10.1, 3.0,  7.5,  73.5, 80.1, 80.0, 56.0, 45.8, 90.4,
+                                             10.1, 3.0,  7.5,  8.7,  23.4, 4.2,  30.5, 18.5, 56.2, 62.4,
+                                             45.8, 9.1,  23.7, 7.3,  34.4, 35.8, 45.8, 52.6, 17.5, 23.6 };
 
-    for (int iterator = 0; iterator < static_cast<int>(temperature_reading.size()); iterator++)
-    {
-        REQUIRE(isBatteryParameterWithinLimits(temperature_reading.at(iterator), min_val, max_val) == true);
-    }
+  for (int iterator = 0; iterator < static_cast<int>(temperature_reading.size()); iterator++)
+  {
+    REQUIRE(isBatteryParameterWithinLimits(temperature_reading.at(iterator), min_val, max_val) == true);
+  }
 }
 TEST_CASE("Test case for string to vector")
 {
-    std::vector<int> expected_battery_parameter_readings = {10, 20, 41, 52};
-    int battery_current = 95;
-    std::string receivedString =
-        "Sending Battery/Charging parameters: 10 V, 20 A Sending Battery/Charging parameters: 41 V, 52 A";
-    REQUIRE(expected_battery_parameter_readings == batteryParameterReadingsFromString(receivedString));
+  std::vector<int> expected_battery_parameter_readings = { 10, 20, 41, 52 };
+  int battery_current = 95;
+  std::string received_string =
+      "Sending Battery/Charging parameters: 10 V, 20 A Sending Battery/Charging parameters: 41 V, 52 A";
+  REQUIRE(expected_battery_parameter_readings == batteryParameterReadingsFromString(received_string));
 }
 
 TEST_CASE("Test case for checking number of Received Battery paramter")
@@ -33,10 +33,16 @@ TEST_CASE("Test case for checking number of Received Battery paramter")
 
 TEST_CASE("Test case for max in given set of Battery paramters")
 {
+  std::vector<int> battery_parameter_readings = { 10, 20, 41, 52, 5, 2, 7, 58, 48, 5 };
+  std::vector<int> expected_output = { 48, 58 };
+  REQUIRE(expected_output == maximumOfBatteryParameterReadings(received_string));
 }
 
 TEST_CASE("Test case for min in given set of Battery paramters")
 {
+  std::vector<int> battery_parameter_readings = { 10, 20, 41, 52, 5, 2, 7, 58, 48, 5 };
+  std::vector<int> expected_output = { 5, 2 };
+  REQUIRE(expected_output == minimumOfBatteryParameterReadings(received_string));
 }
 
 TEST_CASE("Test case Moving Average ")
