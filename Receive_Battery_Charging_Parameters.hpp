7fc4badae212b3c66ee6d2b55ffa4d0687c46ac7
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-23 00:09:01 +0530

    Create Receive_Battery_Charging_Parameters.hpp

---
+++ Receive_Battery_Charging_Parameters.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef RECEIVE_BATTRERY_CHARGING_PARAMETERS
+#define RECEIVE_BATTRERY_CHARGING_PARAMETERS
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+
+
+
+
+
+#endif
