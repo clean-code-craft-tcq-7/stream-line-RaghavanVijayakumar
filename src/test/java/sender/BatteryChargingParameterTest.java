5eacfd2ca1ec40e5a2cef21e3a1ce869aa6c7331
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2023-02-22 18:42:48 +0530

    Update BatteryChargingParameterTest.java

--- src/test/java/sender/BatteryChargingParameterTest.java
+++ src/test/java/sender/BatteryChargingParameterTest.java
@@ -1,10 +1,8 @@
 package sender;
 
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BatteryChargingParameterTest {
 
