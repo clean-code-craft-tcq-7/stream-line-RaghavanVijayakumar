b7d3569167c5fde855a9b3031d56c33e12376b45
deepakp-31 <deepakp3101@gmail.com>
2023-02-23 01:24:01 +0530

    Added string to vector

--- Receive_Battery_Charging_Parameters.cpp
+++ Receive_Battery_Charging_Parameters.cpp
@@ -1,1 +1,42 @@
 #include "Receive_Battery_Charging_Parameters.hpp"
+
+std::vector<int> batteryParameterReadingsFromString(std::string f_received_string)
+{
+  std::stringstream received_string_stream;
+  // std::string f_received_string = " Sending Battery/Charging parameters: 10 V, 20 A Sending Battery/Charging
+  // parameters: 41 V, 52 A";
+
+  // store the string to string stream
+  received_string_stream << f_received_string;
+
+  // declare vector
+  std::vector<int> battery_parameter_readings;
+
+  std::string temporary_string;
+  int number;
+  while (!received_string_stream.eof())
+  {
+    // for getting each word in the string
+    received_string_stream >> temporary_string;
+
+    // check for integers
+    if (std::stringstream(temporary_string) >> number)
+    {
+      // if it is an integer value will be pushed to the vector
+      battery_parameter_readings.push_back(number);
+    }
+  }
+  return battery_parameter_readings;
+}
+
+bool isBatteryParameterWithinLimits(int f_variable, int f_minval, int f_max_val)
+{
+  if (f_variable < f_minval || f_variable > f_max_val)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
--- Receive_Battery_Charging_Parameters.hpp
+++ Receive_Battery_Charging_Parameters.hpp
@@ -4,12 +4,12 @@
 #define RECEIVE_BATTRERY_CHARGING_PARAMETERS
 
 #include <iostream>
-#include <vector>
 #include <random>
+#include <sstream>
+#include <vector>
 
+std::vector<int> batteryParameterReadingsFromString(std::string f_received_string);
 
-
-
-
+bool isBatteryParameterWithinLimits(int f_varible, int f_minval, int f_max_val);
 
 #endif
