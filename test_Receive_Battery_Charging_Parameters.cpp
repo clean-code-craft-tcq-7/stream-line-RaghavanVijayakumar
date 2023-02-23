80822ada0c80da00c51ea869f15ab111083439c7
deepakp-31 <deepakp3101@gmail.com>
2023-02-23 21:12:44 +0530

    updated the last test case

--- test_Receive_Battery_Charging_Parameters.cpp
+++ test_Receive_Battery_Charging_Parameters.cpp
@@ -75,15 +75,12 @@ TEST_CASE("Test case for min in given set of Battery paramters")
 
 TEST_CASE("Test case Moving Average ")
 {
-  int number_of_parameters = 2;
-  int position_of_paramter1{ 1 }, position_of_paramter2{ 2 };
-  std::vector<int> battery_parameter_readings = { 10, 20, 41, 52, 5, 2, 7, 58, 48, 5 };
+  std::vector<int> battery_parameter_readings1 = { 20, 52, 2, 58, 5 };
+  std::vector<int> battery_parameter_readings2 = { 10, 41, 5, 7, 48, 5 };
   int expected_output1 = 5, expected_output2 = 2;
-  int range_length =5;
+  int range_length = 5;
 
-  REQUIRE(expected_output1 == movingAverage(separatingBatteryParameters(battery_parameter_readings,
-                                                                        position_of_paramter1, number_of_parameters),range_length));
+  REQUIRE(expected_output1 == movingAverage(battery_parameter_readings1, range_length));
 
-  REQUIRE(expected_output2 == movingAverage(separatingBatteryParameters(battery_parameter_readings,
-                                                                        position_of_paramter2, number_of_parameters),range_length));
+  REQUIRE(expected_output2 == movingAverage(battery_parameter_readings2, range_length));
 }
